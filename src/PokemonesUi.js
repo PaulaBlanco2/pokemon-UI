cd3c8089e3430142dd73cce479648f5ce279aacb
Pau <blancopaula48@gmail.com>
2024-10-28 14:19:26 -0500

    cambios ui

--- src/PokemonesUi.js
+++ src/PokemonesUi.js
@@ -5,6 +5,7 @@ import '@bbva-experience-components/bbva-button-default/bbva-button-default.js';
 import '@bbva-experience-components/bbva-type-text/bbva-type-text.js';
 import '@bbva-web-components/bbva-spinner/bbva-spinner.js';
 import {BbvaCoreIntlMixin} from '@bbva-web-components/bbva-core-intl-mixin';
+import "@pokedex/pokemones-dm/pokemones-dm.js"
 
 /**
  * ![LitElement component](https://img.shields.io/badge/litElement-component-blue.svg)
@@ -36,6 +37,11 @@ export class PokemonesUi extends BbvaCoreIntlMixin(LitElement) {
     this.loading = true;
   }
 
+  async firstUpdated(){
+    const pokemonesDm =this.shadowRoot.querySelector("pokemones-dm");
+    this.pokemonData = await pokemonesDm.fetchPokemon(),
+  }
+
   static get styles() {
     return [
       styles,
@@ -43,39 +49,9 @@ export class PokemonesUi extends BbvaCoreIntlMixin(LitElement) {
     ];
   }
 
-  async fetchPokemon() {
-    try {
-      console.log('Iniciando fetch de Pokémon');
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=50');
-
-      if (!response.ok) {
-        throw new Error(`Error ${response.status}: ${response.statusText}`);
-      }
-
-      const data = await response.json();
-      const pokemonList = await Promise.all(data.results.map(async (pokemon) => {
-        const res = await fetch(pokemon.url);
-        const pokemonDetails = await res.json();
-        return {
-          nombre: pokemonDetails.name,
-          imagen: pokemonDetails.sprites.front_default,
-          tipos: pokemonDetails.types.map(typeInfo => typeInfo.type.name).join(', '),
-        };
-      }));
-
-      this.pokemonData = pokemonList;
-      this.loading = false;
-    } catch (error) {
-      console.error('Error al obtener datos de Pokémon:', error);
-      this.loading = false;
-    }
-  }
 
   connectedCallback() {
     super.connectedCallback();
-    if (this.active) {
-      this.fetchPokemon();
-    }
   }
 
   render() {
@@ -102,6 +78,8 @@ export class PokemonesUi extends BbvaCoreIntlMixin(LitElement) {
           </div>
         `}
     </div>
+
+    <pokemones-dm></pokemones-dm>
     `;
 
   }
