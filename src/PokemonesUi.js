cac1999dcf082dd8ea6fb9ba9489ff40cde502d6
Pau <blancopaula48@gmail.com>
2024-10-29 14:34:27 -0500

    componentes

--- src/PokemonesUi.js
+++ src/PokemonesUi.js
@@ -1,54 +1,57 @@
-import { CellsPage } from '@cells/cells-page';
-import { html } from 'lit-element';
-import '../../../pokemones-ui/src/PokemonesUi.js';
-import '../../../pokemones-dm/src/PokemonesDm.js';
-import '@bbva-experience-components/bbva-button-default/bbva-button-default.js';
+import { LitElement, html } from 'lit-element';
+import { BbvaCoreIntlMixin } from '@bbva-web-components/bbva-core-intl-mixin';
+import '@pokedex/pokemones-dm/pokemones-dm.js';
 
-class PokemonesPage extends CellsPage {
-  static get is() {
-    return 'pokemones-page';
+export class PokemonesUi extends BbvaCoreIntlMixin(LitElement) {
+  static get properties() {
+    return {
+      pokemonData: { type: Array },
+      loading: { type: Boolean },
+    };
   }
 
   constructor() {
     super();
-    this.selectedPokemon = null;
+    this.pokemonData = [];
+    this.loading = true;
+  }
+
+  firstUpdated() {
+    // Escucha el evento para cargar Pokémon
+    this.addEventListener('pokemon-data-loaded', this.handlePokemonDataLoaded.bind(this));
+    const dm = this.shadowRoot.querySelector('pokemones-dm');
+    if (dm) {
+      dm.fetchPokemon(); // Llama a la función para obtener Pokémon
+    }
+  }
+
+  handlePokemonDataLoaded(event) {
+    this.pokemonData = event.detail;
+    this.loading = false; // Cambia el estado de carga
+    this.requestUpdate(); // Solicita actualización del renderizado
   }
 
   render() {
     return html`
       <div class="main-container">
-        ${this.pokemonData.map(
-          (pokemon) => html`
-            <div class="card">
-              <h3>${pokemon.nombre}</h3>
-              <img src="${pokemon.imagen}" alt="${pokemon.nombre}">
-              <p>Tipos: ${pokemon.tipos}</p>
-              <bbva-button-default
-                text="Evoluciones"
-                @click="${() => this.dispatchEvent(new CustomEvent('show-evolutions', { detail: { pokemon }, bubbles: true, composed: true }))}">
-              </bbva-button-default>
-            </div>
-          `
-        )}
+        ${this.loading 
+          ? html`<bbva-spinner></bbva-spinner>` 
+          : this.pokemonData.length === 0 
+            ? html`<div>No se encontraron Pokémon.</div>`
+            : this.pokemonData.map(pokemon => html`
+                <div class="card">
+                  <h3>${pokemon.nombre}</h3>
+                  <img src="${pokemon.imagen}" alt="${pokemon.nombre}">
+                  <p>Tipos: ${pokemon.tipos}</p>
+                  <bbva-button-default
+                    text="Evoluciones"
+                    @click="${() => this.dispatchEvent(new CustomEvent('show-evolutions', { detail: { pokemon }, bubbles: true, composed: true }))}">
+                  </bbva-button-default>
+                </div>
+              `)
+        }
       </div>
+      <pokemones-dm></pokemones-dm>
     `;
   }
-  
-
-  handleShowEvolutions(event) {
-    const pokemon = event.detail.pokemon;
-    const evolutions = this.getEvolutions(pokemon);
-    this.goToEvolution(evolutions);
-  }
-
-  getEvolutions(pokemon) {
-    return [`${pokemon.nombre} Evolución 1`, `${pokemon.nombre} Evolución 2`];
-  }
-
-  goToEvolution(evolutions) {
-    this.publish('evolutions-channel', { detail: evolutions });
-    this.navigate('evoluciones');
-  }
 }
-
-window.customElements.define(PokemonesPage.is, PokemonesPage);
